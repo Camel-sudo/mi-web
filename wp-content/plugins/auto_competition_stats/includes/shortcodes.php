39e2aa170eb8e9c529d6693b2b73f8e651bb09af
Campe04 <academicaruben@gmail.com>
2024-10-29 23:16:11 +0100

    plugin vFinal

--- wp-content/plugins/auto_competition_stats/includes/shortcodes.php
+++ wp-content/plugins/auto_competition_stats/includes/shortcodes.php
@@ -7,12 +7,12 @@ function acs_display_random_cars() {
     $cars = $wpdb->get_results("SELECT * FROM $table_name ORDER BY RAND() LIMIT 3");
 
     if ($cars) {
-        $output = '<div class="car-gallery" style="display: flex; justify-content: space-between; flex-wrap: wrap; gap: 20px;">';
+        $output = '<div class="car-gallery" style="display: flex; justify-content: center; flex-wrap: wrap; gap: 50px;">';
         
         foreach ($cars as $car) {
-            $output .= '<div class="card" style="border: 1px solid #ccc; border-radius: 8px; padding: 16px; width: 30%; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">';
+            $output .= '<div class="card" style="border: 1px solid #ccc; border-radius: 8px; padding: 16px; width: 500px; box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);">';
             if ($car->image_url) {
-                $output .= "<img src='{$car->image_url}' alt='{$car->model}' style='width:100%; height:auto; border-radius: 8px; margin-bottom: 8px;'/>";
+                $output .= "<img src='{$car->image_url}' alt='{$car->model}' style='width: 100%; height: 300px; object-fit: cover; border-radius: 8px; margin-bottom: 8px;' />";
             }
             $output .= "<h2>{$car->model}</h2>
                         <h3>Historial de Competiciones</h3>
@@ -21,7 +21,7 @@ function acs_display_random_cars() {
                         <p>{$car->technical_specifications}</p>
                         <h3>Estadísticas de Éxitos</h3>
                         <p>{$car->success_statistics}</p>
-                    </div>"; // cierra .card
+                    </div>";
         }
         
         $output .= '</div>'; // cierra .car-gallery
@@ -31,3 +31,4 @@ function acs_display_random_cars() {
     }
 }
 add_shortcode('random_cars', 'acs_display_random_cars');
+?>
