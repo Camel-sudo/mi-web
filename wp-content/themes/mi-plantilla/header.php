7cdd065b9c0fce0d1d0e6f7c9f0d8bc7722bc54b
Camel-sudo <yeyator05@gmail.com>
2024-10-29 12:53:03 +0100

    v1.1

--- wp-content/themes/mi-plantilla/header.php
+++ wp-content/themes/mi-plantilla/header.php
@@ -1,9 +1,20 @@
 <!DOCTYPE html>
-<html lang="en">
+<html <?php language_attributes(); ?>>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="modernizr.js"></script>
-    <script src="respond.min.js"></script>
-    <title>Document</title>
+<meta charset= <?php bloginfo( 'charset' ); ?>">
+<title><?php wp_title(); ?></title>
+<!-- Definimos el viewport para dispositivos móviles -->
+<meta name="viewport" content="width=device-width, minimum-scale=1">
+<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();
+?>/favicon.ico" />
+<link rel="stylesheet" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+<?php wp_head(); ?>
 </head>
+<body>
+<div class="wrapper">
+<header>
+<h1><a href="<?php echo get_option('home'); ?>"><?php bloginfo('name'); ?></a></h1>
+<hr>
+<?php wp_nav_menu( array('menu' => 'Main', 'container' => 'nav' )); ?>
+</header>
