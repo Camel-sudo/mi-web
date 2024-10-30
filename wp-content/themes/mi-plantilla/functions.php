1bf2bfa5ffa298814a60ec172b6a8f298621d3af
administrador <administrador@dwcs.cotarelo.ga>
2024-10-30 10:31:05 +0100

    v1.2

--- wp-content/themes/mi-plantilla/functions.php
+++ wp-content/themes/mi-plantilla/functions.php
@@ -17,4 +17,14 @@ register_sidebar(array(
 'before_title' => '<h3>',
 'after_title' => '</h3>',
 ));
+//Habilitar thumbnails
+add_theme_support('post-thumbnails');
+set_post_thumbnail_size(200, 150, true);
+// Permitir comentarios encadenados
+function enable_threaded_comments(){
+    if (is_singular() AND comments_open() AND (get_option('thread_comments') == 1)) {
+    wp_enqueue_script('comment-reply');
+    }
+    }
+    add_action('get_header', 'enable_threaded_comments');
 ?>
