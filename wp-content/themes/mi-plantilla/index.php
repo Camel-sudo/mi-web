3e6819fd8f6601464474391c607e502fffbea900
Camel-sudo <Yeyator05@gmail.com>
2024-11-03 16:36:10 +0100

    finiquitao

--- wp-content/themes/mi-plantilla/index.php
+++ wp-content/themes/mi-plantilla/index.php
@@ -5,6 +5,12 @@
     <!-- <?php   echo do_shortcode('[random_cars]'); ?> -->
 </section> <!-- Fin de main -->
 <?php get_sidebar()?>
+<?php $post = $posts[0]; ?>
+<?php if (is_category()) { ?>
+<h2>Categoría: <?php get_the_category(); ?></h2>
+<?php } elseif( is_tag() ) { ?>
+<h2>Etiqueta: <?php single_tag_title(); ?></h2>
+<?php } ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
  <small>Publicado el <?php the_time('j/m/Y') ?> por <?php
--- wp-content/themes/mi-plantilla/tags.php
+++
@@ -1,6 +0,0 @@
-<?php $post = $posts[0]; ?>
-<?php if (is_category()) { ?>
-    <h2>Categoría: <?php single_cat_title(); ?></h2>
-<?php } elseif (is_tag()) { ?>
-    <h2>Etiqueta: <?php single_tag_title(); ?></h2>
-<?php } ?>
