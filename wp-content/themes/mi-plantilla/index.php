e899becf0f886d09ae89855b2dc0e60d6f47ec08
Camel-sudo <Yeyator05@gmail.com>
2024-11-03 15:38:25 +0100

    1

--- wp-content/themes/mi-plantilla/index.php
+++ wp-content/themes/mi-plantilla/index.php
@@ -1,10 +1,10 @@
 <?php get_header(); ?>
 <section id="main">
+
     <img src="<?php echo get_template_directory_uri(); ?>/images/volkswagen_corrado_vr6_9.jpg" alt="FERNANDO ALONSO">
     <!-- <?php   echo do_shortcode('[random_cars]'); ?> -->
 </section> <!-- Fin de main -->
 <?php get_sidebar()?>
-<?php get_footer(); ?>
 <?php if (have_posts()) : while (have_posts()) : the_post(); ?>
  <h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
  <small>Publicado el <?php the_time('j/m/Y') ?> por <?php
@@ -20,3 +20,4 @@ the_author_posts_link() ?> </small>
  <?php endwhile; else: ?>
  <p><?php _e('No hay entradas .'); ?></p>
  <?php endif; ?>
+ <?php get_footer(); ?>
